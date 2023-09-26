add2566b4e5db9519139d6fe2f94eab0d7792121
BuiHieu123 <buihieu1262003@gmail.com>
2023-09-26 15:26:43 +0700

    test

---
+++ js/brand.js
@@ -0,0 +1,92 @@
+ // URL of the API you want to call
+ const apiUrl = 'http://localhost:8080/rest/brand';
+
+ // Get a reference to the table
+ const table = document.getElementById('data-table');
+ const tbody = table.querySelector('tbody');
+
+ // Make the API request using fetch
+ fetch(apiUrl)
+     .then(response => response.json())
+     .then(data => {
+         // Clear any existing data in the table body
+         tbody.innerHTML = '';
+
+         // Loop through the data from the API and add it to the table
+         data.forEach(item => {
+             const row = document.createElement('tr');
+             row.innerHTML = `
+         <td>${item.id}</td>
+         <td>${item.name}</td>
+         <td>${item.status === 1 ? 'Hoạt động' : 'Không hoạt động'}</td>
+         <td>
+                                                <div class="dropdown no-arrow" style="width: 1px;">
+                                                    <a class="dropdown-toggle" role="button" id="dropdownMenuLink"
+                                                        data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                        <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-700"></i>
+                                                    </a>
+                                                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                                                        aria-labelledby="dropdownMenuLink">
+                                                        <a class="dropdown-item" href="" data-toggle="modal"
+                                                            data-target="#hihiModal">Sửa Thông Tin</a>
+                                                            <a class="dropdown-item" href="">Xóa</a>
+                                                    </div>
+                                                </div>
+                                            </td>
+     `;
+             tbody.appendChild(row);
+         });
+     })
+     .catch(error => {
+         console.error('Error while calling the API:', error);
+     });
+ // Hàm thêm dữ liệu từ form vào table
+ document.getElementById('myForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = {
+        name: document.getElementById('name').value,
+        status: document.getElementById('status').value
+    };
+
+    // Gọi API để thêm dữ liệu vào cơ sở dữ liệu
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            alert("Thêm dữ liệu thành công!");
+            
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+       
+});
+const khoangThoiGian = 50000; // 5000 millisecond = 5 giây
+
+function taiLaiTrang() {
+  location.reload(); // Tải lại trang hiện tại
+}
+setInterval(taiLaiTrang, khoangThoiGian);
+
+
+
+
+
+
+
+
+ 
+  
+  
+  
+  
+  
+  
