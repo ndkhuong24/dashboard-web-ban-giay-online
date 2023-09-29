aab88c749a76643e3dc0bb8cd6ff35305bda7f4c
BuiHieu123 <buihieu1262003@gmail.com>
2023-09-29 14:34:21 +0700

    update

--- js/brand.js
+++ js/brand.js
@@ -1,24 +1,24 @@
- // URL of the API you want to call
- const apiUrl = 'http://localhost:8080/rest/brand';
+// URL of the API you want to call
+const apiUrl = 'http://localhost:8080/api/brand';
 
- // Get a reference to the table
- const table = document.getElementById('data-table');
- const tbody = table.querySelector('tbody');
+// Get a reference to the table
+const table = document.getElementById('data-table');
+const tbody = table.querySelector('tbody');
 
- // Make the API request using fetch
- fetch(apiUrl)
-     .then(response => response.json())
-     .then(data => {
-         // Clear any existing data in the table body
-         tbody.innerHTML = '';
+// Make the API request using fetch
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        // Clear any existing data in the table body
+        tbody.innerHTML = '';
 
-         // Loop through the data from the API and add it to the table
-         data.forEach(item => {
-             const row = document.createElement('tr');
-             row.innerHTML = `
+        // Loop through the data from the API and add it to the table
+        data.forEach(item => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
          <td>${item.id}</td>
          <td>${item.name}</td>
-         <td>${item.status === 1 ? 'Hoạt động' : 'Không hoạt động'}</td>
+         <td>${item.status == 1 ? 'Hoạt động' : 'Không hoạt động'}</td>
          <td>
                                                 <div class="dropdown no-arrow" style="width: 1px;">
                                                     <a class="dropdown-toggle" role="button" id="dropdownMenuLink"
@@ -35,14 +35,14 @@
                                                 </div>
                                             </td>
      `;
-             tbody.appendChild(row);
-         });
-     })
-     .catch(error => {
-         console.error('Error while calling the API:', error);
-     });
- // Hàm thêm dữ liệu từ form vào table
- document.getElementById('myForm').addEventListener('submit', function (event) {
+            tbody.appendChild(row);
+        });
+    })
+    .catch(error => {
+        console.error('Error while calling the API:', error);
+    });
+// Hàm thêm dữ liệu từ form vào table
+document.getElementById('myForm').addEventListener('submit', function (event) {   
     event.preventDefault();
 
     const formData = {
@@ -52,6 +52,7 @@
 
     // Gọi API để thêm dữ liệu vào cơ sở dữ liệu
     fetch(apiUrl, {
+        
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -62,19 +63,42 @@
         .then(data => {
             console.log(data);
             alert("Thêm dữ liệu thành công!");
-            
+
         })
         .catch(error => {
             console.error('Error:', error);
         });
-       
+
 });
-const khoangThoiGian = 50000; // 5000 millisecond = 5 giây
+//Hàm Update từ form vào table:
+document.getElementById('myForm2').addEventListener('submit', function (event) {   
+    event.preventDefault();
 
-function taiLaiTrang() {
-  location.reload(); // Tải lại trang hiện tại
-}
-setInterval(taiLaiTrang, khoangThoiGian);
+    const formData = {
+        name: document.getElementById('name').value,
+        status: document.getElementById('status').value
+    };
+
+    // Gọi API để thêm dữ liệu vào cơ sở dữ liệu
+    fetch('http://localhost:8080/rest/brand/{id}', {
+        
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            alert("Update dữ liệu thành công!");
+
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+});
 
 
 
@@ -83,10 +107,11 @@ setInterval(taiLaiTrang, khoangThoiGian);
 
 
 
- 
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
