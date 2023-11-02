cff35efbd6c848fb987605afc5c988e29e81ac89
ndkhuong24 <kn134646@gmail.com>
2023-11-02 15:54:26 +0700

    Update Product

--- js/product.js
+++ js/product.js
@@ -8,55 +8,8 @@ let currentPage = 1;
 
 let data = [];
 
-let totalPages = 1;
-
 const prevButton = document.getElementById("prev-button");
 const nextButton = document.getElementById("next-button");
-
-document.addEventListener("DOMContentLoaded", function () {
-  var selectElement = document.getElementById("style_id");
-
-  fetch("https://192.168.109.128/api/Style/active")
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((item) => {
-        var option = document.createElement("option");
-        option.value = item.id;
-        option.text = item.name;
-        selectElement.appendChild(option);
-      });
-    })
-    .catch((error) => {
-      console.error("Lỗi khi tải dữ liệu từ API: " + error);
-    });
-});
-
-function GetStyleNameById(styleId) {
-  // Thực hiện yêu cầu GET để lấy thông tin Style từ API Style dựa trên style_id
-  fetch(`https://192.168.109.128/api/Style/id/${styleId}`)
-    .then((response) => response.json())
-    .then((styleData) => {
-      // Hiển thị tên Style trong cột tương ứng của bảng
-      const styleNameCell = document.getElementById(`style-name-${styleId}`);
-      styleNameCell.textContent = styleData.name;
-    })
-    .catch((error) => {
-      console.error("Lỗi khi gọi API Style:", error);
-    });
-}
-
-function GetStyleIdByName(style_name) {
-  const apiUrl = `https://192.168.109.128/api/Style/name/${style_name}`;
-  return fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      return data.id;
-    })
-    .catch((error) => {
-      console.error("Lỗi khi gọi API:", error);
-      return null;
-    });
-}
 
 function renderTable(data, page) {
   tbody.innerHTML = "";
@@ -82,9 +35,20 @@ function renderTable(data, page) {
 
     var styleIDNow = parseInt(item.style_id);
     GetStyleNameById(styleIDNow);
-
     tbody.appendChild(row);
   }
+}
+
+function GetStyleNameById(styleId) {
+  fetch(`https://192.168.109.128/api/Style/id/${styleId}`)
+    .then((response) => response.json())
+    .then((styleData) => {
+      const styleNameCell = document.getElementById(`style-name-${styleId}`);
+      styleNameCell.textContent = styleData.name;
+    })
+    .catch((error) => {
+      console.error("Lỗi khi gọi API Style:", error);
+    });
 }
 
 function fetchDataAndPopulateTable() {
@@ -106,8 +70,6 @@ function updatePageInfo() {
   currentPageElement.textContent = `${currentPage}/${totalPages}`;
 }
 
-fetchDataAndPopulateTable();
-
 prevButton.addEventListener("click", () => {
   if (currentPage > 1) {
     currentPage--;
@@ -124,62 +86,7 @@ nextButton.addEventListener("click", () => {
   }
 });
 
-table.addEventListener("click", function (event) {
-  if (event.target.classList.contains("btn-secondary")) {
-    $("#confirmationModal").modal("show");
-
-    // Xác nhận cập nhật khi người dùng nhấn nút "Xác nhận" trong modal
-    document
-      .getElementById("confirmUpdate")
-      .addEventListener("click", async function () {
-        const clickedRow = event.target.closest("tr");
-        const cells = clickedRow.querySelectorAll("td");
-        const id = parseInt(cells[0].textContent);
-        const code = cells[1].textContent;
-        const name = cells[2].textContent;
-        const style_name = cells[3].textContent;
-        const description = cells[4].textContent;
-        const create_date = cells[5].textContent;
-        const status = cells[6].textContent;
-        const newStatus = status === "Hoạt động" ? 0 : 1;
-        const style_id = await GetStyleIdByName(style_name);
-
-        var dataToUpdate = {
-          id: id,
-          code: code,
-          name: name,
-          style_id: style_id,
-          description: description,
-          create_date: create_date,
-          status: newStatus,
-        };
-
-        const requestOptions = {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(dataToUpdate),
-        };
-
-        fetch(apiUrl, requestOptions)
-          .then((response) => {
-            if (response.ok) {
-              tbody.innerHTML = "";
-              fetchDataAndPopulateTable();
-            } else {
-              response.text().then((data) => {
-                console.log("Lỗi: " + data);
-              });
-            }
-          })
-          .catch((error) => {
-            console.log("Lỗi: " + error.message);
-          });
-        $("#confirmationModal").modal("hide");
-      });
-  }
-});
+fetchDataAndPopulateTable();
 
 document.getElementById("saveChanges").addEventListener("click", function () {
   const code = document.getElementById("code").value;
@@ -258,3 +165,69 @@ function searchByName(searchPattern) {
       console.error("Lỗi khi gọi API:", error);
     });
 }
+
+table.addEventListener("click", function (event) {
+  if (event.target.classList.contains("btn-secondary")) {
+    const clickedRow = event.target.closest("tr");
+    if (clickedRow) {
+      const Styleid = clickedRow.querySelector("td:first-child").textContent;
+      document.getElementById("modalProductId").value = Styleid;
+      $("#confirmationModal").modal("show");
+    }
+  }
+});
+
+function fetchProductById(styleId, callback) {
+  const apiUrl = `https://192.168.109.128/api/Product/${styleId}`;
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Lỗi khi gọi API");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      callback(data);
+    })
+    .catch((error) => {
+      console.error("Lỗi: " + error);
+    });
+}
+
+var dataToUpdate;
+
+document.getElementById("confirmUpdate").addEventListener("click", function () {
+  const productIdModal = document.getElementById("modalProductId").value;
+  fetchProductById(productIdModal, function (productData) {
+    dataToUpdate = {
+      id: productData.id,
+      code: productData.code,
+      name: productData.name,
+      style_id: productData.style_id,
+      description: productData.description,
+      create_date: productData.create_date,
+      status: productData.status === 1 ? 0 : 1,
+    };
+    fetch(apiUrl, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataToUpdate),
+    })
+      .then((response) => {
+        if (response.ok) {
+          tbody.innerHTML = "";
+          fetchDataAndPopulateTable();
+        } else {
+          response.text().then((data) => {
+            console.log("Lỗi: " + data);
+          });
+        }
+      })
+      .catch((error) => {
+        console.log("Lỗi: " + error.message);
+      });
+    $("#confirmationModal").modal("hide");
+  });
+});
