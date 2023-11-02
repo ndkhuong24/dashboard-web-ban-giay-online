79aebe9f18e72b6853b6e9afe9ff93848778eba8
ndkhuong24 <kn134646@gmail.com>
2023-11-02 17:23:22 +0700

    Update

--- js/product-detail.js
+++ js/product-detail.js
@@ -165,3 +165,75 @@ function insertProductDetail() {
     alert("Vui lòng điền đầy đủ thông tin và chọn hình ảnh.");
   }
 }
+
+const table = document.getElementById("data-table");
+const tbody = table.querySelector("tbody");
+const perPage = 5;
+let currentPage = 1;
+let data = [];
+const prevButton = document.getElementById("prev-button");
+const nextButton = document.getElementById("next-button");
+
+function renderTable(data, page) {
+  tbody.innerHTML = "";
+
+  const startIndex = (page - 1) * perPage;
+  const endIndex = page * perPage;
+
+  for (let i = startIndex; i < endIndex && i < data.length; i++) {
+    const item = data[i];
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${item.id}</td>
+      <td>
+        <img src="https://192.168.109.128${item.path}" style="width: 100px;height: 100px;">
+      </td>
+      <td>${item.productName}</td>
+      <td>${item.styleName}</td>
+      <td>${item.quantity}</td>
+      <td>${item.price}</td>
+      <td>${item.status == 1 ? "Hoạt động" : "Không hoạt động"}</td>
+      <td>
+        <button class="btn btn-secondary">Cập nhật</button>
+      </td>
+    `;
+    tbody.appendChild(row);
+  }
+}
+
+function fetchDataAndPopulateTable() {
+  fetch("https://192.168.109.128/api/ProductDetail/getAll")
+    .then((response) => response.json())
+    .then((apiData) => {
+      data = apiData;
+      totalPages = Math.ceil(data.length / perPage);
+      updatePageInfo();
+      renderTable(data, currentPage);
+    })
+    .catch((error) => {
+      console.error("Lỗi khi gọi API:", error);
+    });
+}
+
+function updatePageInfo() {
+  const currentPageElement = document.getElementById("currentPage");
+  currentPageElement.textContent = `${currentPage}/${totalPages}`;
+}
+
+prevButton.addEventListener("click", () => {
+  if (currentPage > 1) {
+    currentPage--;
+    renderTable(data, currentPage);
+    updatePageInfo();
+  }
+});
+
+nextButton.addEventListener("click", () => {
+  if (currentPage < totalPages) {
+    currentPage++;
+    renderTable(data, currentPage);
+    updatePageInfo();
+  }
+});
+
+fetchDataAndPopulateTable();
