eae049a751c53002a9a7a7a1f6793df387d5c936
ndkhuong24 <kn134646@gmail.com>
2023-11-03 10:31:05 +0700

    Update Product Detail

--- js/product-detail.js
+++ js/product-detail.js
@@ -2,9 +2,18 @@ const anhChinh = document.getElementById("anhChinh");
 var anhChinhPreview = document.getElementById("anh-chinh-preview");
 const anhPhu = document.getElementById("anhPhu");
 var anhPhuPreview = document.getElementById("anh-phu-preview");
-
+var notification = document.getElementById("notification");
+var notificationText = document.getElementById("notification-text");
 var anhChinhData;
 var anhPhuData;
+
+function showNotification(message) {
+  notificationText.textContent = message;
+  notification.style.display = "block";
+  setTimeout(function () {
+    notification.style.display = "none";
+  }, 3000);
+}
 
 anhChinh.addEventListener("change", function (event) {
   anhChinhPreview.innerHTML = "";
@@ -19,7 +28,7 @@ anhChinh.addEventListener("change", function (event) {
 anhPhu.addEventListener("change", function (event) {
   var files = event.target.files;
   if (files.length > 4) {
-    alert("Bạn chỉ có thể tải tối đa 4 ảnh");
+    showNotification("Bạn chỉ có thể tải tối đa 4 ảnh");
     event.target.value = "";
     return;
   }
@@ -51,10 +60,10 @@ document.getElementById("saveChanges").addEventListener("click", function (e) {
       })
       .catch((error) => {
         console.log("Lỗi: " + error.message);
-        alert("Lỗi khi tải ảnh chính lên");
+        showNotification("Lỗi khi tải ảnh chính lên");
       });
   } else {
-    alert("Vui lòng tải ảnh chính lên");
+    showNotification("Vui lòng tải ảnh chính lên");
   }
 });
 
@@ -79,10 +88,10 @@ function uploadanhPhu() {
       })
       .catch((error) => {
         console.log("Lỗi: " + error.message);
-        alert("Lỗi khi tải ảnh phụ lên");
+        showNotification("Lỗi khi tải ảnh phụ lên");
       });
   } else {
-    alert("Vui lòng tải ảnh phụ lên");
+    showNotification("Vui lòng tải ảnh phụ lên");
   }
 }
 
@@ -97,7 +106,6 @@ function insertProductDetail() {
   var quantity = document.getElementById("quantity").value;
   var price = document.getElementById("price").value;
 
-  // Kiểm tra xem "quantity" có phải là số nguyên và lớn hơn 0 không
   if (
     anhChinhData &&
     anhPhuData &&
@@ -112,16 +120,15 @@ function insertProductDetail() {
     price
   ) {
     if (!/^\d+$/.test(quantity) || parseInt(quantity) <= 0) {
-      alert("Số lượng phải là số nguyên lớn hơn 0.");
-      return; // Không tiếp tục thực hiện nếu số lượng không hợp lệ
+      showNotification("Số lượng phải là số nguyên lớn hơn 0.");
+      return;
     }
 
     if (!/^\d+$/.test(quantity) || parseInt(price) <= 0) {
-      alert("Đơn giá phải là số nguyên lớn hơn 0.");
-      return; // Không tiếp tục thực hiện nếu số lượng không hợp lệ
+      showNotification("Đơn giá phải là số nguyên lớn hơn 0.");
+      return;
     }
 
-    // Nếu số lượng hợp lệ, tiếp tục xử lý và gửi dữ liệu
     var dataToInsert = {
       categoryID: categoryID,
       brandID: brandID,
@@ -137,20 +144,18 @@ function insertProductDetail() {
     };
     var jsonData = JSON.stringify(dataToInsert);
 
-    // Tạo một đối tượng cấu hình cho yêu cầu fetch
     var requestOptions = {
       method: "POST",
-      body: jsonData, // Dữ liệu JSON để thêm
+      body: jsonData,
       headers: {
-        "Content-Type": "application/json", // Đặt tiêu đề Content-Type là "application/json"
+        "Content-Type": "application/json",
       },
     };
 
-    // Gửi yêu cầu HTTP POST sử dụng fetch
     fetch("https://192.168.109.128/api/ProductDetail/insert", requestOptions)
       .then((response) => {
         if (response.ok) {
-          alert("Dữ liệu đã được thêm thành công.");
+          showNotification("Dữ liệu đã được thêm thành công.");
           location.reload();
         } else {
           response.text().then((data) => {
@@ -162,7 +167,7 @@ function insertProductDetail() {
         console.log("Lỗi: " + error.message);
       });
   } else {
-    alert("Vui lòng điền đầy đủ thông tin và chọn hình ảnh.");
+    showNotification("Vui lòng điền đầy đủ thông tin và chọn hình ảnh.");
   }
 }
 
@@ -186,7 +191,9 @@ function renderTable(data, page) {
     row.innerHTML = `
       <td>${item.id}</td>
       <td>
-        <img src="https://192.168.109.128${item.path}" style="width: 100px;height: 100px;">
+        <img src="https://192.168.109.128${
+          item.path
+        }" style="width: 100px;height: 100px;">
       </td>
       <td>${item.productName}</td>
       <td>${item.styleName}</td>
@@ -237,3 +244,27 @@ nextButton.addEventListener("click", () => {
 });
 
 fetchDataAndPopulateTable();
+
+document.getElementById("searchButton").addEventListener("click", function () {
+  const searchPattern = document.getElementById("searchInput").value;
+  if (searchPattern.trim() === "") {
+    fetchDataAndPopulateTable();
+  } else {
+    searchByName(searchPattern);
+  }
+});
+
+function searchByName(searchPattern) {
+  fetch(`https://192.168.109.128/api/ProductDetail/searchName/${searchPattern}`)
+    .then((response) => response.json())
+    .then((searhData) => {
+      currentPage = 1;
+      data = searhData;
+      totalPages = Math.ceil(data.length / perPage);
+      updatePageInfo();
+      renderTable(data, currentPage);
+    })
+    .catch((error) => {
+      console.error("Lỗi khi gọi API:", error);
+    });
+}
